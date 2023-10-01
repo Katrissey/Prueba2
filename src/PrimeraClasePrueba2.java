a64490a2fbf4ebdaf94cba4f580bee617cd4fc64
Katrissey <rggilkatris@gmail.com>
2023-10-02 00:48:06 +0200

    Segunda prueba

---
+++ src/PrimeraClasePrueba2.java
@@ -0,0 +1,7 @@
+
+public class PrimeraClasePrueba2 {
+	/*Ahora es el primer comentario de bloque que escribo desde eclipse en mi ordenador
+	 * para comprobar que migra correctamente a Github.
+	 */
+
+}
